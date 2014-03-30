3d14cf7f3d132288eda0dde29d1af013f781e0b4
Atul Varma <varmaa@gmail.com>
2014-03-30 18:23:35 -0400

    added experimental named-routes.js.

---
+++ test/named-routes.test.js
@@ -0,0 +1,33 @@
+var should = require('should');
+
+var NamedRoutes = require('../').NamedRoutes;
+
+describe('NamedRoutes', function() {
+  it('should work w/ routes that have no parameters', function() {
+    var routes = new NamedRoutes();
+    routes.add('/foo', 'foo');
+    routes.reverse('foo').should.eql('/foo');
+  });
+
+  it('should work w/ routes that have parameters', function() {
+    var routes = new NamedRoutes();
+    routes.add('/foo/:id', 'foo');
+    routes.reverse('foo', {id: 'bar'}).should.eql('/foo/bar');
+  });
+
+  it('should raise errors on nonexistent routes', function() {
+    var routes = new NamedRoutes();
+    (function() {
+      routes.reverse('foo');
+    }).should.throw('Unknown route name "foo"');
+  });
+
+  it('should raise errors on insufficient params', function() {
+    var routes = new NamedRoutes();
+    routes.add('/foo/:id', 'foo');
+
+    (function() {
+      routes.reverse('foo', {});
+    }).should.throw('Missing param "id" for route "foo"');
+  });
+});
