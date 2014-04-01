09ec42bc1bace02f3dea923b9d13c40a19da4586
Atul Varma <varmaa@gmail.com>
2014-04-01 10:34:30 -0400

    made fieldwidget template tag update label,input,textarea elements.

--- test/model-field-widget.test.js
+++ test/model-field-widget.test.js
@@ -3,26 +3,66 @@ var nunjucks = require('nunjucks');
 
 var WidgetExtension = require('../').module('./model-field-widget');
 
+function buildEnv() {
+  var env = new nunjucks.Environment();
+
+  WidgetExtension.initialize(env);
+  return env;
+}
+
 describe('model field widget', function() {
-  it('should work', function() {
-    var env = new nunjucks.Environment();
+  it('should throw error when field data type is unsupported', function() {
+    (function() {
+      buildEnv().renderString('{% fieldwidget f %}{% endfieldwidget %}', {
+        f: {basename: 'funk', type: 'Funky'}
+      });
+    }).should.throw(/no widget for data type "Funky" for field "funk"/);
+  });
+});
 
-    WidgetExtension.initialize(env);
+describe('model field widget for String', function() {
+  var env, ctx;
 
-    var html = env.renderString([
-      '{% fieldwidget myfield %}',
-      'hi {{field.name}}',
-      '{% endfieldwidget %}',
-      '{{field.name}}'
-    ].join(''), {
+  function testTransform(start, end) {
+    if (Array.isArray(start)) start = start.join('');
+    if (Array.isArray(end)) end = end.join('');
+    env.renderString(start, ctx).should.eql(end);
+  }
+
+  beforeEach(function() {
+    env = buildEnv();
+    ctx = {
       myfield: {
-        name: 'NAME',
-        transformHTML: function(html) {
-          return 'begin ' + html + ' end';
-        }
+        type: 'String',
+        name: 'myname',
+        id: 'myid',
+        value: 'myvalue'
       }
-    });
+    };
+  });
 
-    html.should.eql('begin hi NAME end');
+  it('should work with <input>', function() {
+    testTransform([
+      '{% fieldwidget myfield %}',
+      '<label>this is {{field.name}}</label>',
+      '<input type="text">',
+      '{% endfieldwidget %}',
+      '{{field.name}}' // Also ensure 'field' is unset after block tag end.
+    ], [
+      '<label for="myid">this is myname</label>',
+      '<input type="text" id="myid" name="myname" value="myvalue">'
+    ]);
+  });
+
+  it('should work with <textarea>', function() {
+    testTransform([
+      '{% fieldwidget myfield %}',
+      '<label>this is {{field.name}}</label>',
+      '<textarea></textarea>',
+      '{% endfieldwidget %}',
+    ], [
+      '<label for="myid">this is myname</label>',
+      '<textarea id="myid" name="myname">myvalue</textarea>'
+    ]);
   });
 });
