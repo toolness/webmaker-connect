cd7a43a924a35adc0b5c0952f1c6359dd564eba8
Atul Varma <varmaa@gmail.com>
2014-04-01 08:19:08 -0400

    added tests for ModelForm.

---
+++ test/model-form.test.js
@@ -0,0 +1,77 @@
+var Schema = require('mongoose').Schema;
+var should = require('should');
+
+var db = require('./db');
+var ModelForm = require('../').module('./model-form');
+
+var fooSchema = new Schema({
+  name: {type: String, required: true},
+  funkyValidator: {type: String, validate: [function(val) {
+    return val == 'funky';
+  }, 'This is not funky.']},
+  defaulted: {type: String, default: function() { return 'default!'; }},
+  privateInfo: String
+});
+var Foo = db.model('Foo', fooSchema);
+
+describe('ModelForm', function() {
+  beforeEach(db.wipe);
+
+  it('ultimately falls back to a well-defined default', function() {
+    var form = new ModelForm({model: Foo, fields: ['privateInfo']});
+    form.field('privateInfo').value.should.equal('');
+  });
+
+  it('takes field values from schema defaults', function() {
+    var form = new ModelForm({model: Foo, fields: ['defaulted']});
+    form.field('defaulted').value.should.eql('default!');
+  });
+
+  it('has expected field properties', function() {
+    var form = new ModelForm({model: Foo, fields: ['name']});
+    var field = form.field('name');
+
+    field.basename.should.eql('name');
+    field.name.should.eql('Foo_name');
+    field.id.should.eql('Foo_name');
+    field.isRequired.should.be.true;
+  });
+
+  it('takes whitelisted values from form data', function(done) {
+    var foo = new Foo({name: 'u', privateInfo: 'unsullied'});
+    var form = new ModelForm({
+      model: Foo,
+      instance: foo,
+      formData: {Foo_name: 'blah', Foo_privateInfo: 'evil'},
+      fields: ['name']
+    });
+    form.validateAndSave(function() {
+      should.equal(form.errors, null);
+      should.equal(form.field('name').error, null);
+      Foo.find(function(err, foos) {
+        if (err) return done(err);
+        foos.length.should.eql(1);
+        foos[0].name.should.eql('blah');
+        foos[0].privateInfo.should.equal('unsullied');
+        done();
+      });
+    });
+  });
+
+  it('reports errors for required fields', function(done) {
+    var form = new ModelForm({model: Foo, fields: ['name']});
+    form.validateAndSave(function() {
+      form.field('name').error.should.eql('This field is required.');
+      done();
+    });
+  });
+
+  it('reports errors from custom validators', function(done) {
+    var form = new ModelForm({model: Foo, fields: ['funkyValidator']});
+    form.validateAndSave(function() {
+      form.field('funkyValidator').error.should.eql('This is not funky.');
+      done();
+    });
+  });
+
+});
