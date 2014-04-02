6a85d080eeafbf3ba80844987fe49b70c4305720
Atul Varma <varmaa@gmail.com>
2014-04-02 09:10:25 -0400

    added NamedRoutes.reverseAbsolute().

--- test/named-routes.test.js
+++ test/named-routes.test.js
@@ -2,7 +2,7 @@ var should = require('should');
 
 var NamedRoutes = require('../').NamedRoutes;
 
-describe('NamedRoutes', function() {
+describe('NamedRoutes', function() {  
   it('should work w/ routes that have no parameters', function() {
     var routes = new NamedRoutes();
     routes.add('/foo', 'foo');
@@ -30,6 +30,20 @@ describe('NamedRoutes', function() {
     routes.add('/foo/:id', 'foo');
     routes.reverse('foo', {id: '50'}).should.eql('/foo/50');
     consulted.should.eql(1);
+  });
+
+  it('should provide absolute URLs when properly configured', function() {
+    var routes = new NamedRoutes('http://example.org');
+    routes.add('/foo', 'foo');
+    routes.baseURL.should.eql('http://example.org');
+    routes.reverseAbsolute('foo').should.eql('http://example.org/foo');
+  });
+
+  it('should raise error when no base URL is configured', function() {
+    var routes = new NamedRoutes();
+    (function() {
+      routes.reverseAbsolute('foo');
+    }).should.throw('Base URL is not defined');
   });
 
   it('should raise errors on nonexistent routes', function() {
