3280cf19e657d9de58f2fba0ba398e10e3155614
Atul Varma <varmaa@gmail.com>
2014-04-01 20:28:17 -0400

    replaced janky _ROUTE_PARAM_ hack w/ parameter handlers.

--- test/named-routes.test.js
+++ test/named-routes.test.js
@@ -16,10 +16,20 @@ describe('NamedRoutes', function() {
     routes.reverse('foo', {id: 1}).should.eql('/foo/1');
   });
 
-  it('should namespace routes with _ROUTE_PARAM_ if needed', function() {
+  it('should consult param handler first', function() {
     var routes = new NamedRoutes();
+    routes.registerHandler('id', function(obj) { return obj.meh; });
     routes.add('/foo/:id', 'foo');
-    routes.reverse('foo', {_ROUTE_PARAM_id: 'bar'}).should.eql('/foo/bar');
+    routes.reverse('foo', {meh: 'bop', id: '1'}).should.eql('/foo/bop');
+  });
+
+  it('should fallback to property lookups after param handler', function() {
+    var routes = new NamedRoutes();
+    var consulted = 0;
+    routes.registerHandler('id', function() { consulted++; });
+    routes.add('/foo/:id', 'foo');
+    routes.reverse('foo', {id: '50'}).should.eql('/foo/50');
+    consulted.should.eql(1);
   });
 
   it('should raise errors on nonexistent routes', function() {
