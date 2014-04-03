0a7fde4872869007c98d997ea5e6687cc81d0ad5
Atul Varma <varmaa@gmail.com>
2014-04-03 10:55:39 -0400

    added signedOAuthRequestMiddleware and validNonceAndTimestampMiddleware.

--- test/oauth-util.test.js
+++ test/oauth-util.test.js
@@ -1,8 +1,125 @@
 var should = require('should');
 var express = require('express');
+var hmacsign = require('oauth-sign').hmacsign;
 var request = require('supertest');
 
 var oauthUtil = require('../').oauthUtil;
+
+describe('oauthUtil.signedOAuthRequestMiddleware()', function() {
+  var middleware = oauthUtil.signedOAuthRequestMiddleware;
+
+  function app(oauth, getSecrets) {
+    oauth.oauth_signature_method = 'HMAC-SHA1';
+    return express()
+      .use(function(req, res, next) { req.oauth = oauth; next(); })
+      .use(middleware({
+        baseURL: 'http://example.org',
+        getSecrets: getSecrets
+      }))
+      .get('*', function(req, res) { return res.send(200); });
+  }
+
+  it('should reject invalid keys/tokens', function(done) {
+    request(app({
+      oauth_consumer_key: 'lol',
+      oauth_token: 'wut',
+    }, function getSecrets(options, cb) {
+      options.consumerKey.should.eql('lol');
+      options.accessToken.should.eql('wut');
+      cb(null, null);
+    })).get('/')
+      .expect('invalid consumer key or access token')
+      .expect(401, done);
+  });
+
+  it('should reject invalid signatures', function(done) {
+    request(app({
+      oauth_consumer_key: 'lol',
+      oauth_token: 'wut',
+      oauth_signature: 'totally invalid.',
+      oauth_verifier: 'hmm'
+    }, function getSecrets(options, cb) {
+      should.equal(options.accessToken, null);
+      cb(null, {consumer: 'hey'});
+    })).get('/')
+      .expect('invalid signature')
+      .expect(401, done);
+  });
+
+  it('should accept valid signatures', function(done) {
+    var signature = hmacsign('GET', 'http://example.org/boop', {
+      foo: 'lol',
+      z: 'z',
+      oauth_signature_method: 'HMAC-SHA1',
+      oauth_version: '1.0A',
+      oauth_timestamp: 1396492044,
+      oauth_nonce: 'u',
+      oauth_consumer_key: 'lol'
+    }, 'consumersecret', 'atsecret');
+
+    signature.should.eql('AGKLZGlxw4+GjNRHBwkRs3Ivbm8=');
+    request(app({
+      oauth_version: '1.0A',
+      oauth_timestamp: '1396492044',
+      oauth_nonce: 'u',
+      oauth_consumer_key: 'lol',
+      oauth_signature: signature
+    }, function getSecrets(options, cb) {
+      cb(null, {consumer: 'consumersecret', accessToken: 'atsecret'});
+    })).get('/boop?foo=lol&z=z')
+      .expect(200, done);
+  });
+});
+
+describe('oauthUtil.validNonceAndTimestampMiddleware()', function() {
+  var middleware = oauthUtil.validNonceAndTimestampMiddleware;
+  var usedNonces = [];
+
+  beforeEach(function() { usedNonces = []; });
+
+  function app(oauth) {
+    return express()
+      .use(function(req, res, next) { req.oauth = oauth; next(); })
+      .use(middleware(function check(options, cb) {
+        var key = [options.consumerKey, options.nonce,
+                   options.timestamp].join(':');
+        if (usedNonces.indexOf(key) != -1)
+          return process.nextTick(function() { cb(null, false); });
+        usedNonces.push(key);
+        process.nextTick(function() { cb(null, true); });
+      }))
+      .get('/', function(req, res) { return res.send(200); });
+  }
+
+  it('should pass through valid key/nonce/timestamp', function(done) {
+    request(app({
+      oauth_consumer_key: 'lol',
+      oauth_nonce: 'wut',
+      oauth_timestamp: '123'
+    })).get('/').expect(200, function(err) {
+      if (err) return done(err);
+      usedNonces.should.eql(['lol:wut:123']);
+      done();
+    });
+  });
+
+  it('should reject invalid timestamp', function(done) {
+    request(app({oauth_timestamp: '0'})).get('/')
+      .expect('invalid timestamp')
+      .expect(401, done);
+  });
+
+  it('should reject used key/nonce/timestamp', function(done) {
+    usedNonces.push('lol:wut:123');
+    request(app({
+      oauth_consumer_key: 'lol',
+      oauth_nonce: 'wut',
+      oauth_timestamp: '123'
+    })).get('/')
+      .expect('invalid nonce or timestamp')
+      .expect(401, done);
+  });
+});
 
 describe('oauthUtil.notBadOAuthRequestMiddleware()', function() {
   var VALIDATORS = oauthUtil.VALIDATORS;
