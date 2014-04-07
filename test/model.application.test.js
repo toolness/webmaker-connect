74ecadd206cec73ab4dd8a10f97d7c8374326334
Atul Varma <varmaa@gmail.com>
2014-04-07 08:40:42 -0400

    added Application.findAuthorizedByUser().

--- test/model.application.test.js
+++ test/model.application.test.js
@@ -2,17 +2,20 @@ var should = require('should');
 
 var db = require('./db');
 var Application = require('../').module('./model/application');
+var Tokens = require('../').module('./model/tokens');
+
+var exampleApp = {
+  name: 'my cool app',
+  description: 'this is my cool app yo',
+  website: 'http://coolapp.com',
+  owner: 'foo'
+};
 
 describe('Application', function() {
   beforeEach(db.wipe);
 
   it('should auto-generate api key and secret on save', function(done) {
-    (new Application({
-      name: 'my cool app',
-      description: 'this is my cool app yo',
-      website: 'http://coolapp.com',
-      owner: 'foo'
-    })).save(function(err, model) {
+    (new Application(exampleApp)).save(function(err, model) {
       if (err) return done(err);
       model.apiKey.should.match(/^([A-Za-z0-9]+)$/);
       model.apiSecret.should.match(/^([A-Za-z0-9]+)$/);
@@ -41,5 +44,55 @@ describe('Application', function() {
       should.equal(err.errors.callbackURL, undefined);
       done();
     });
+  });
+});
+
+describe('Application.findAuthorizedByUser', function(done) {
+  var application, token;
+
+  beforeEach(db.wipe);
+  beforeEach(function(done) {
+    application = new Application(exampleApp);
+    application.save(done);
+  });
+  beforeEach(function(done) {
+    token = new Tokens.AccessToken({
+      token: 'lol',
+      user: {userId: 'foo'},
+      application: application._id
+    });
+    token.save(done);
+  });
+
+  it('should return apps that user has access tokens for', function(done) {
+    Application.findAuthorizedByUser('foo', function(err, apps) {
+      if (err) return done(err);
+      apps.should.have.length(1);
+      apps[0].name.should.eql('my cool app');
+      done();
+    });
+  });
+
+  it('should not return apps that user hasn\'t authorized', function(done) {
+    Application.findAuthorizedByUser('bar', function(err, apps) {
+      if (err) return done(err);
+      apps.should.have.length(0);
+      done();
+    });
+  });
+
+  it('should not return duplicate entries', function(done) {
+    new Tokens.AccessToken({
+      token: 'dup',
+      user: {userId: 'foo'},
+      application: application._id
+    }).save(function(err) {
+      if (err) return done(err);
+      Application.findAuthorizedByUser('foo', function(err, apps) {
+        if (err) return done(err);
+        apps.should.have.length(1);
+        done();
+      });
+    });    
   });
 });
